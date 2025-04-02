1dc1383cd66053af6697d1f50a0fd9943fc7fd6d
Jasur Dustmurodov <jasur.coder@gmail.com>
2025-04-02 11:49:54 +0500

    laravel 12 installed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,15 +2,12 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use OverflowException;
-use RuntimeException;
 
 /**
- * @extends Factory<User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
@@ -23,8 +20,6 @@ class UserFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
-     *
-     * @throws RuntimeException|OverflowException
      */
     public function definition(): array
     {
